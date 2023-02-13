382016e7f71db2f3eb4d552444105f11c892b903
Elrashede <ahmedelrashede1998@gmail.com>
2023-02-14 00:46:46 +0200

    call api from react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-
+import quote from './component/quote';
+import axios, { all } from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+
+  const getQuote = () => {
+     
+    axios.get(`http://localhost:5000/quote/random`)
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data)
+      });
+     console.log(data)
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Quote</h1><br></br>
+      <button
+        type="submit"
+        className="btn btn-primary mb-4"
+       onClick={getQuote}
+      >
+        Get Quote
+      </button>
+
+
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>QuoteId</th>
+            <th>Quote</th>
+            <th>Author</th>
+
+          </tr>
+        </thead>
+        <tbody>
+
+          <tr>
+            <td>{data.quoteId}</td>
+            <td>{data.quote}</td>
+            <td>{data.author}</td>
+          </tr>
+
+        </tbody>
+      </table>
     </div>
   );
 }
